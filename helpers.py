cdef50632b2ee6c744962629a6c3e9c9ae6c82d7
Mark Turner <mark.turner-7@postgrad.manchester.ac.uk>
2021-06-23 16:22:13 +0100

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+def getChromium(path):
+    if path.startswith("/usr/"): #if os is linux
+        f = open(path,'r')
+        content = f.read()
+        f.close()
+        return re.search('"last_chrome_version":"(.*)"',content).groups[0]
+    if path.startswith("/Applications/"): #is os is macos
+        return os.readlink(path) #works via symlink to "Current" as being the latest installed
+    return os.listdir(path)[0]
+
+def getBrave(path):
+    if path.startswith("/usr/"):
+        f = open(path,'r')
+        content = f.read()
+        f.close()
+        return re.search('"last_chrome_version":"(.*)"',content).groups[0]
+    return os.listdir(path)[0]
+
+def getFirefox(path):
+    f = open(path,'r')
+    content = f.read() 
+    f.close()
+    return re.search("Version=(.*)",content).groups()[0]
---
+++ main.py
@@ -0,0 +1,81 @@
+print("Importing libaries...",end='\t')
+from helpers import *
+import socket
+import psutil
+import platform
+import os
+import subprocess
+import json
+
+print("[OK]")
+
+def onFail(critical=False):
+	print("[FAIL]")
+	if critical: 
+		print("Critical Error! Exiting...")
+		exit(0)
+
+def main():	
+    print("Scanning OS...",end="\t")
+    try:
+        os = platform.system().lower()
+        if os == "darwin":
+            os = "macos"
+        print("[OK]")
+    except:
+        onFail(True)
+
+    print("Scanning software...")
+    installed = {}
+    f = open("paths.json",'r')
+    software = f.read()
+    f.close()
+    software = json.loads(software)
+    
+    for i in software.keys():
+        try:
+            if i == "brave":
+                installed[i] = getBrave(software.get(i)[os])
+            elif i == "chrome" or i == "edge":
+                installed[i] = getChromium(software.get(i)[os])
+            elif i == "firefox" or i == "tor":
+                installed[i] = getFirefox(software.get(i)[os])
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            installed[i] = False
+
+    print("Getting OS version...",end='\t')
+    try:
+        if os == "macos":
+            osVer = subprocess.run(["sw_vers","-buildVersion"], capture_output=True).stdout.decode()[:-1]
+        elif os == "windows": #Windows
+            osVer = platform.platform()
+        else: #Linux
+            osVer = subprocess.run(["uname","-v"], capture_output=True).stdout.decode()[:-1]
+        installed["osVer"] = osVer
+        print("[OK]")
+    except:
+        onFail()
+
+    print("Phoning home...",end="\t")
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("17.253.37.203", 80))
+        s.send("GET / HTTP/1.1\r\nHost:captive.apple.com\r\n\r\n".encode())
+        handshake = s.recv(4096).decode()
+        if "Success" in handshake: 
+            print("[OK]")
+        else: 
+            print("Connection error.") 
+    except:
+        onFail(True)
+
+    print("Sending collected data...",end='\t')
+
+    print("[OK]")
+    
+    print(installed)
+
+if __name__ == '__main__':
+	main()
